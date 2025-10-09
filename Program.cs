3513f95be7d7a9dc1b89471e30785614959a08b9
Winnerty <timofey.n.karpeev@gmail.com>
2025-10-09 11:50:01 +0200

    added env file

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@ using LibraryApp.Data;
 using LibraryApp.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 namespace LibraryApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Env.Load();
+
             var builder = WebApplication.CreateBuilder(args);
-            var connectionString = builder.Configuration.GetConnectionString("LibraryAppContextConnection") ?? throw new InvalidOperationException("Connection string 'LibraryAppContextConnection' not found."); ;
+            var connectionString = builder.Configuration.GetConnectionString("LibraryAppContextConnection")
+            ?? throw new InvalidOperationException("Connection string 'LibraryAppContextConnection' not found."); ;
 
             builder.Services.AddDbContext<LibraryAppContext>(options => options.UseSqlServer(connectionString));
 
